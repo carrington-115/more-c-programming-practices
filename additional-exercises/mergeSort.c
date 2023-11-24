4308a1e7fb8a3c675cf5006d773ce61485e7d0e6
Mark Carrington <frumarkcarrington@gmail.com>
2023-11-24 08:10:19 +0530

    fixed some files errors

--- additional-exercises/mergeSort.c
+++ additional-exercises/mergeSort.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void){
     // before the sorting of the array
     int numbers[] = {};
     int size_of_array = sizeof(numbers)/sizeof(numbers[0]);
     printf("\nThis is the array before the sorting: \n");
+
+    // merge sorting process
+    mergeSortAlgorithm(numbers, size);
 
     // after sorting the array
     printf("Sorted Array: \n");
@@ -15,8 +19,14 @@ int main(void){
     return 0;
 }
 
-void mergeSortAlgorithm(int array[]){
+void mergeSortAlgorithm(int array[], int size){
+    int array_divisions = size / 2;
+    int sub_arrays[][];
+    int size_tracker = 0;
 
+    // dividing the array ... 
+    do{
+        
 
-
+    }while(array_divisions != 2);
 }
