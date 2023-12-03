c5272ce01b58b63e3d86484a891ada98a132f0a3
Mark Carrington <frumarkcarrington@gmail.com>
2023-12-04 03:54:21 +0530

    Started a program that entails the revision of pointers and learning memory allocation in c programming Firstly learning the different types of pointers in c and how to use them for dynamic programming

---
+++ additional-exercises/memory_allocation_and_pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+int main(){
+    int *null_pointer = NULL; // The null pointer
+    int numbers[5] = {1, 2, 3, 4, 5}; 
+    int *array = numbers; // array pointers
+    int *ptr; // void pointer
+
+
+
+
+}
