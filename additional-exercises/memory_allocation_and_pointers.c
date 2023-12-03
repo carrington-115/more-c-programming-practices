54272ba298e759086ab6158a8d9fa479a6f6acb1
Mark Carrington <frumarkcarrington@gmail.com>
2023-12-04 04:25:14 +0530

    Learnt how to use function pointers

--- additional-exercises/memory_allocation_and_pointers.c
+++ additional-exercises/memory_allocation_and_pointers.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 
+int addition(int a, int b){
+    return (a + b);
+}
+
+// how ot use function pointer in c programming
+int (*addFuncPtr)(int, int) = &addition;
+
 
 int main(){
     int *null_pointer = NULL; // The null pointer
@@ -8,6 +15,9 @@ int main(){
     int *array = numbers; // array pointers
     int *ptr; // void pointer
 
+    
+    printf("The sum of 2 and 4 is: %d", addFuncPtr(2, 4));
+    return 0;
 
 
 
