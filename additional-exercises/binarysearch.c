ae5d4a103b932660aede1eb29326470add1c20c4
Mark Carrington <frumarkcarrington@gmail.com>
2023-12-04 09:49:51 +0530

    Corrected the bug on the bubble sorting algorithm All it needed was an if statement that checks if the j value is less that length-1 variable

--- additional-exercises/binarysearch.c
+++ additional-exercises/binarysearch.c
@@ -24,71 +24,12 @@
 void sort_with_bubble_sort(int array[], int length);
 
 int main(void){
-    int numbers[] = {5, 2, 9, 1, 7, 4, 8, 3, 6, 10};
+    int numbers[] = {5, 2, 9, 1, 7, 4, 8, 3, 6, 10, 30, 45, 200, 67, 93};
     int length = sizeof(numbers)/sizeof(numbers[0]);
     int i, j, temp;
 
 
     sort_with_bubble_sort(numbers, length);
-
-    // // printing the array before sorting
-    // printf("Array before sorting: \n");
-    // for (i = 0; i < 10; i++){
-    //     printf("%d ", array[i]);
-    // }
-    // // sorting the array with bubble sort
-    // for (i = 0; i < 10; i++){
-    //     for (j = 0; j < 10; j++){
-    //         if (array[j] > array[j+1]){
-    //             temp = array[j];
-    //             array[j] = array[j+1];
-    //             array[j+1] = temp; 
-    //         }
-    //     }
-    // }
-
-    // // printing the arrray after sorting
-    // printf("\nArray after sorting: \n");
-    // for (i = 0; i < 10; i++){
-    //     printf("%d ", array[i]);
-    // }
-
-    // int number;
-    // printf("\nEnter the number to be searched: ");
-    // scanf("%d", &number);
-
-    // // binary searching algorithm
-    
-    // // getting the size of the array
-    // int length = sizeof(array)/sizeof(array[0]);
-    // int middle = length/2;
-
-    // // the search algorithm
-    // int middle_element = array[middle];
-
-    // // if middle element is the search element
-    // if (middle_element == number){
-    //     printf("%d found at position %d", number, middle);
-
-    // }
-    // else if (middle_element < number){
-    //     while (true){
-    //         middle_element = array[middle]; 
-    //         while(middle_element != number){
-    //             if (middle_element == number){
-    //                 printf("\n%d found at position %d", number, middle);
-    //                 break;
-    //             }
-    //             else if (middle_element < number){
-    //                 middle = (length - middle)/2;
-    //             }
-    //             else if(middle_element > number)
-    //         }
-            
-    //     }
-    // }
-
-
 
     return 0;
 }
@@ -107,12 +48,14 @@ void sort_with_bubble_sort(int array[], int length){
     // beginning the sorting algorithm
     for (i = 0; i < length; i++){
         for (j = 0; j < length; j++){
-            element = array[j];
-            check_element = array[j+1];
-            if (element > check_element){
+            if (j < length-1){
+                element = array[j];
+                check_element = array[j+1];
+                if (element > check_element){
                 swap = element;
                 array[j] = check_element;
                 array[j+1] = swap;
+                }
             }
         }
     }
