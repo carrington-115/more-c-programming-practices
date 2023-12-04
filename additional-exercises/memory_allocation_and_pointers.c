8054a5bdecec41414ef2354ab98691deb061cafb
Mark Carrington <frumarkcarrington@gmail.com>
2023-12-04 07:26:05 +0530

    Updated the program

--- additional-exercises/memory_allocation_and_pointers.c
+++ additional-exercises/memory_allocation_and_pointers.c
@@ -8,6 +8,12 @@ int addition(int a, int b){
 // how ot use function pointer in c programming
 int (*addFuncPtr)(int, int) = &addition;
 
+int *createArray(int a, int b){
+    int numbers[2] = {a, b};
+    int *array = numbers;
+    return *array;
+}
+
 
 int main(){
     int *null_pointer = NULL; // The null pointer
@@ -17,6 +23,27 @@ int main(){
 
     
     printf("The sum of 2 and 4 is: %d", addFuncPtr(2, 4));
+
+    printf("\n");
+    // printing addresses
+    for (int i = 0; i < (sizeof(numbers)/sizeof(numbers[0])); i++){
+        printf("%d ", array[i]);
+    }
+
+    // printing values
+    printf("\n");
+    for (int i = 0; i < (sizeof(numbers)/sizeof(numbers[0])); i++){
+        printf("%d ", &array[i]);
+    }
+
+    // using array with functions
+    printf("\nUsing array with functions part\n");
+    int new_array = *createArray(2, 6);
+    for (int i = 0; i < (sizeof(new_array)/sizeof(new_array[0])); i++){
+        printf("%d ", new_array[i]);
+    }
+
+
     return 0;
 
 
